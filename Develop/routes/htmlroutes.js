66ee62fa84bee047bfd54f6369c2e6249747e843
zsadov1 <zsadov1@gmail.com>
2020-03-18 15:55:23 -0400

    FIxing bugs

--- Develop/routes/htmlroutes.js
+++ Develop/routes/htmlroutes.js
@@ -1,6 +1,10 @@
 const path = require("path");
 const router = require("express").Router();
 
+  router.get("/", function(req,res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  })
+
   router.get("/exercise", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/exercise.html"));
   });
