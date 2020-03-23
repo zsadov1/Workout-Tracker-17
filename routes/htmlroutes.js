9341b9271f4b9e21be2a813c43b25722c29641b7
zsadov1 <zsadov1@gmail.com>
2020-03-23 12:45:02 -0400

    Route Fixes

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -2,15 +2,15 @@ const path = require("path");
 const router = require("express").Router();
 
   router.get("/", function(req,res) {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
+    res.sendFile(path.join(__dirname, "index.html"));
   })
 
   router.get("/exercise", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+    res.sendFile(path.join(__dirname, "exercise.html"));
   });
 
   router.get("/stats", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/stats.html"));
+    res.sendFile(path.join(__dirname, "stats.html"));
   });
 
   module.exports = router;
